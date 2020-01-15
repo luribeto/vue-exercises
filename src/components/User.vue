4cbf251657defda3d80d81413b0c159be1d8b72c
Leonardo Uribe <luribeto@gmail.com>
2020-01-15 16:22:13 -0500

    complete props and emitBus before assignment 7

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,13 +2,16 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <p>Name is: {{ name }}</p>
+        <p>Age is: {{ age }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :name="name" @nameResetted="name = $event" :resetName="resetName" :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age" @ageEditted="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
@@ -19,6 +22,20 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function() {
+            return {
+                name: 'Leo',
+                age: 27,
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'David';
+            },
+            resetName() {
+                this.name = 'Leo';
+            },
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,41 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetNameChild">Reset name</button>
+        <button @click="resetName">Reset name callback</button>
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../main';
+
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true,
+                default: 'Leo'
+            },
+            resetName: Function,
+            userAge: Number,
+        },
+        methods: {
+            switchName() {
+                return this.name.split('').reverse().join('')
+            },
+            resetNameChild() {
+                this.name = 'Leo';
+                this.$emit('nameResetted', this.name);
+            }
+        },
+        created() {
+            eventBus.$on('ageEditted', (newAge) => {
+                this.userAge = newAge
+            })
+        }
+    }
 </script>
 
 <style scoped>
