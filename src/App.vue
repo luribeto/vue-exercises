3b79aa1679673bdf30010346e8e8d34dd0259ef2
Leonardo Uribe <luribeto@gmail.com>
2020-01-16 15:33:57 -0500

    slot tag

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
+  <div class="container">
+    <div class="row">
+      <app-quote>
+        <h2>My quote</h2>
+      </app-quote>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './Header.vue';
-    import Footer from './Footer.vue';
-    import Servers from './Servers.vue';
-    import ServerDetails from './ServerDetails.vue';
+  import Quote from './components/Quote.vue';
 
-    export default {
-        components: {
-            'app-header': Header,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+  export default {
+    components: {
+      appQuote: Quote,
     }
+  }
 </script>
 
 <style>
