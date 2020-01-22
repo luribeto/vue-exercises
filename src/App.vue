6d325d2cf28e6e746c78b6321c3ae694687ede6d
Leonardo Uribe <luribeto@gmail.com>
2020-01-22 14:14:29 -0500

    initial quote

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
     <div class="container">
-    
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteGrid from './components/QuoteGrid.vue';
+
+  export default {
+    data: () => ({
+      quotes: [
+        'Initial quote here!'
+      ],
+      maxQuotes: 10,
+    }),
+    components: {
+      "app-quote-grid": QuoteGrid,
     }
+  }
 </script>
 
 <style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        
+      <quote>
+      </quote>
+    </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
